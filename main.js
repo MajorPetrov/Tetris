bc643d0c5e6b0914f56c49e9c89ffef1da4ae766
MajorPetrov <razdobreevvlad@live.fr>
2019-01-18 10:20:14 +0100

    - Mise à jour README - déplacement de l'index dans le dossier app - changement des guillemets dans main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron");
 
 // Gardez une reference globale de l'objet window, si vous ne le faites pas, la fenêtre sera
 // fermée automatiquement quand l'objet JavaScript sera supprimée par le ramasse-miettes.
@@ -9,13 +9,13 @@ function createWindow() {
     win = new BrowserWindow({ width: 800, height: 600 });
 
     // et charge le index.html de l'application.
-    win.loadFile('index.html');
+    win.loadFile("app/index.html");
 
     // Ouvre les DevTools.
     win.webContents.openDevTools();
 
     // Émit lorsque la fenêtre est fermée.
-    win.on('closed', () => {
+    win.on("closed", () => {
         // Dé-référence l'objet window , normalement, vous stockeriez les fenêtres
         // dans un tableau si votre application supporte le multi-fenêtre. C'est le moment
         // où vous devez supprimer l'élément correspondant.
@@ -26,18 +26,18 @@ function createWindow() {
 // Cette méthode sera appelée quant Electron aura fini
 // de s'initialiser et sera prêt à créer des fenêtres de navigation.
 // Certaines APIs peuvent être utilisées uniquement quand cet événement est émit.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quitte l'application quand toutes les fenêtres sont fermées.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
     // Sur macOS, il est commun pour une application et leur barre de menu
     // de rester active tant que l'utilisateur ne quitte pas explicitement avec Cmd + Q
-    if (process.platform !== 'darwin') {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
     // Sur macOS, il est commun de re-créer une fenêtre de l'application quand
     // l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
     if (win === null) {
